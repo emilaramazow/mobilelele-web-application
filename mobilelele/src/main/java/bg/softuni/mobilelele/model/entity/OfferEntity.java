8bc705281032821cdc58be410eef0c02b7a0a083
emilaramazow <emil.aramazow@gmail.com>
2022-05-29 22:58:38 +0300

    Added Engine, Transmission and User-Role enums and Offer,User,User-Role entities

---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/model/entity/OfferEntity.java
@@ -0,0 +1,141 @@
+package bg.softuni.mobilelele.model.entity;
+
+import bg.softuni.mobilelele.model.enums.EngineEnum;
+import bg.softuni.mobilelele.model.enums.TransmissionEnum;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "offers")
+public class OfferEntity {
+
+    /*
+    Offers ID's will be UUID's and primary keys (Just for example)
+     */
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Type(type = "uuid-char")
+    private UUID id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EngineEnum engine;
+    private String imageURL;
+    private int mileAge;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransmissionEnum transmission;
+    @Column(nullable = false)
+    private int year;
+    @ManyToOne
+    private ModelEntity model;
+    @ManyToOne
+    private UserEntity seller;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public OfferEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public OfferEntity setEngine(EngineEnum engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public OfferEntity setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+        return this;
+    }
+
+    public int getMileAge() {
+        return mileAge;
+    }
+
+    public OfferEntity setMileAge(int mileAge) {
+        this.mileAge = mileAge;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public OfferEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public OfferEntity setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public OfferEntity setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public OfferEntity setModel(ModelEntity model) {
+        this.model = model;
+        return this;
+    }
+
+    public UserEntity getSeller() {
+        return seller;
+    }
+
+    public OfferEntity setSeller(UserEntity seller) {
+        this.seller = seller;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "id=" + id +
+                ", engine=" + engine +
+                ", imageURL='" + imageURL + '\'' +
+                ", mileAge=" + mileAge +
+                ", price=" + price +
+                ", transmission=" + transmission +
+                ", year=" + year +
+                ", model=" + model +
+                ", seller=" + seller +
+                '}';
+    }
+}
---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/model/enums/EngineEnum.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobilelele.model.enums;
+
+public enum EngineEnum {
+
+    GASOLINE, DIESEL, ELECTRIC, HYBRID
+
+}
