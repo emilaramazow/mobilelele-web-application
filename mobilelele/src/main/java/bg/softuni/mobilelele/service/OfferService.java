044f085d113480e9784edf5406a99f8625b9210d
emilaramazow <emil.aramazow@gmail.com>
2022-06-18 13:05:51 +0300

    Starting work for addition of offers

---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/service/OfferService.java
@@ -0,0 +1,15 @@
+package bg.softuni.mobilelele.service;
+
+import bg.softuni.mobilelele.model.dto.AddOfferDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferService {
+
+    public void addOffer(AddOfferDTO addOfferDTO) {
+
+        // TODO
+
+    }
+
+}
--- mobilelele/src/main/java/bg/softuni/mobilelele/web/OfferController.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/web/OfferController.java
@@ -1,10 +1,24 @@
 package bg.softuni.mobilelele.web;
 
+import bg.softuni.mobilelele.model.dto.AddOfferDTO;
+import bg.softuni.mobilelele.service.OfferService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class OfferController {
+
+    private OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
 
     @GetMapping("/offers/all")
     public String allOffers() {
@@ -12,7 +26,29 @@ public class OfferController {
     }
 
     @GetMapping("/offers/add")
-    public String addOffer() {
+    public String addOffer(Model model) {
+
+        if (!model.containsAttribute("addOfferModel")) {
+            model.addAttribute("addOfferModel", new AddOfferDTO());
+        }
+
         return "offer-add";
+    }
+
+    @PostMapping("/offers/add")
+    public String addOffer(@Valid AddOfferDTO addOfferModel,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("addOfferModel", addOfferModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.addOfferModel", bindingResult);
+
+            return "redirect:/offers/add";
+        }
+
+        //TODO OfferService
+
+        return "redirect:/";
     }
 }
