144455c45eb32c9fac627db9f4edec76efffc97a
emilaramazow <emil.aramazow@gmail.com>
2022-07-13 22:55:15 +0300

    Commit

--- mobilele/src/main/resources/data.sql
+++ mobilele/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 -- some test users
 INSERT INTO users (id, email, first_name, last_name, image_url, is_active, password)
-VALUES (1, 'lachezar.balev@gmail.com', 'Lucho', 'Balev', null, 1, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2');
+VALUES (1, 'lachezar.balev@gmail.com', 'Lucho', 'Balev', null, 1, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2'),
+ (2, 'emil.aramazow@gmail.com', 'Emil', 'Aramazov', null, 1, '57e7759fd2d59275fc3c3cd5dd2ace5013b39ee972999412f3f5f5c3382b6765c2571ef86648abe2');
 
 
 INSERT INTO brands (id, name)
